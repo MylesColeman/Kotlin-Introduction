fcf0b16366e0d7b39003591f82ebe21b4e701554
Myles Coleman <mylesjcoleman1011@gmail.com>
2026-01-21 12:56:06 +0000

    Feat: Implement list splitter function
    Implemented a function which takes a list of integers, then splits it as evenly as possible into 2 separate lists.

---
+++ ListSplitter.kt
@@ -0,0 +1,20 @@
+fun main() {
+    
+}
+
+fun listSplitter(fullList: listOf<Int>){
+    
+    var listOne = listOf<Int>
+    var listTwo = listOf<Int>
+    var listIndex = Int
+    
+    for (i in fullList.size() / 2){
+        listOne.add[i]
+        
+        listIndex = i
+    }
+    
+    for (i + listIndex in fullList.size(){
+        listOne.add[i + listIndex]
+    }
+}
