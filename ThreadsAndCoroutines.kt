797658d3bdc94ae0dce2afdc93fb14272a4640b0
Myles Coleman <mylesjcoleman1011@gmail.com>
2026-02-18 10:53:42 +0000

    fix(mechanic): fix compiler errors with coroutine code
    Refactored code to be ran in main, ensured the coroutines actually work.

--- ThreadsAndCoroutines.kt
+++ ThreadsAndCoroutines.kt
@@ -1,5 +1,54 @@
-fun main() {
+import kotlinx.coroutines.*
+
+fun main() = runBlocking {
+    /*val thread = SimpleThread()
+	thread.start()
+
+	val threadWithRunnable = Thread(SimpleRunnable())
+	threadWithRunnable.start()*/
+
+	/*val thread = Thread {
+    	println("${Thread.currentThread()} has run.")
+	}
+	thread.start()*/
+
+	/*runBlocking {
+    	val job = launch(Dispatchers.Default) {
+        	println("${Thread.currentThread()} has run.")
+    	}
+	}*/
+
+	/*val job = GlobalScope.launch {
+    	println("${Thread.currentThread()} has run.")
+	}*/
+
+	/*runBlocking {
+    	val job = launch {
+        	println("${Thread.currentThread()} has run.")
+    	}
+	}*/
+
+	/*val deferred = async {
+    	return@async "${Thread.currentThread()} has run."
+	}*/
+
+	/*val deferred = async(Dispatchers.Unconfinded, CoroutineStart.LAZY) {
+    	println("${Thread.currentThread()} has run.")
+    }*/
+		
+	val deferred = async(Dispatchers.IO) {
+    	println("${Thread.currentThread()} has run.")
+	}
     
+    /*deferred.await()*/
+    
+    val result = deferred.await()
+    println(result)
+
+	runBlocking(newSingleThreadContext("dedicatedThread")) {
+    	val result = deferred.await()
+    	println(result)
+	}
 }
 
 /*class SimpleThread: Thread() {
@@ -9,56 +58,7 @@ fun main() {
 }*/
 
 class SimpleRunnable: Runnable {
-   public verride fun run() {
+   override fun run() {
        println("${Thread.currentThread()} has run.")
    } 
-}
-
-/*val thread = SimpleThread()
-thread.start()
-
-val threadWithRunnable = Thread(SimpleRunnable())
-threadWithRunnable.start()*/
-
-/*val thread = Thread {
-    println("${Thread.currentThread()} has run.")
-}
-thread.start()*/
-
-/*runBlocking {
-    val job = launch(Dispatchers.Default) {
-        println("${Thread.currentThread()} has run.")
-    }
-}*/
-
-/*val job = GlobalScope.launch {
-    println("${Thread.currentThread()} has run.")
-}*/
-
-/*runBlocking {
-    val job = launch {
-        println("${Thread.currentThread()} has run.")
-    }
-}*/
-
-val deferred = async {
-    return@async "${Thread.currentThread()} has run."
-}
-
-val deferred = async(Dispatchers.Unconfinded, CoroutineStart.LAZY) {
-    println("${Thread.currentThread()} has run.")
-}
-
-val deferred = async(Dispatchers.IO) {
-    println("${Thread.currentThread()} has run.")
-}
-
-runBlocking {
-    val result = deferred.await()
-    println(result)
-}
-
-runBlocking(newSingleThreadContext("dedicatedThread")) {
-    val result = deferred.await()
-    println(result)
 }
